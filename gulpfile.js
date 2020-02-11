cf1a6b33375def79a91ef9bf59a48b4a75a3d6b2
Antoha5001 <plantsvzzombi@mail.ru>
2020-02-11 18:58:42 +0700

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp = require('gulp');  // инициализация, подключение модуля
+const { src, dest, parallel, watch } = require('gulp');  // инициализация, подключение модуля
 const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
@@ -18,50 +18,55 @@ function hello() {
     return console.log('hello');
 }
 
-function styles() {
-   return gulp.src(cssFiles)
+function css() {
+   return src(cssFiles)
        .pipe(concat('style.css'))
        .pipe(autoprefixer({
            browsers: ['last 2 versions'],
            cascade: false
        }))
        .pipe(cleanCSS({level: 2}))
-       .pipe(gulp.dest('./build/css/'))
+       .pipe(dest('./build/css/'))
        .pipe(browserSync.stream());
 }
 
 function scripts() {
-    return gulp.src(jsFiles)
+    return src(jsFiles)
         .pipe(concat('script.js'))
         .pipe(uglify({toplevel: true}))
-        .pipe(gulp.dest('./build/js/'))
+        .pipe(dest('./build/js/'))
         .pipe(browserSync.stream());
 }
 
-function watch(){
+function watchAll(){
 
     browserSync.init({
-            proxy: "gulp-2018.base"
+            proxy: "gulp.base"
     });
 
-    gulp.watch('./src/css/**/*.css', styles);
-    gulp.watch('./src/js/**/*.js', scripts);
-    gulp.watch('./*.php').on('change', browserSync.reload);
+    watch('./src/css/**/*.css', css);
+    watch('./src/js/**/*.js', scripts);
+    // gulp.watch('./*.php').on('change', browserSync.reload);
+    watch('./*.html').on('change', browserSync.reload);
 }
 
 function clean(){
     return del(['build/*']);
 }
 
-gulp.task('styles', styles);
-gulp.task('scripts', scripts);
-gulp.task('watch', watch);
+// gulp.task('styles', css);
+// gulp.task('scripts', scripts);
+exports.css = css;
+exports.scripts = scripts;
+exports.watch = watchAll;
 
-gulp.task('build', gulp.series(clean, gulp.parallel(styles,scripts)) );
-gulp.task('dev', gulp.series('build', 'watch') );
+// gulp.task('watch', watchAll);
+
+// gulp.task('build', gulp.series(clean, parallel(styles,scripts)) );
+// gulp.task('dev', gulp.series('build', 'watch') );
 
 function defaultTask(cb) {
     cb();
     // console.log('hello');
 }
-exports.default = watch;
+exports.default = watchAll;
